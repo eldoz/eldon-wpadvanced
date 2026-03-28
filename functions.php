abb0ad1d0f282b096fbaa9b74ebd8e9e38074e24
eldoz <81470244+eldoz@users.noreply.github.com>
2026-03-28 06:43:23 -0700

    eee

--- functions.php
+++ functions.php
@@ -12,4 +12,25 @@ functions ds_theme_assets(){
 
 add_action('wp_enqueue_scripts','ds_theme_assets');
 
+function ds_add_bootstrap_cdn(){
+
+    wp_enqueue_style(
+        'bootstrap-css',
+        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css',
+        array('')
+        '4.6.2',
+        'all'
+    );
+
+    wp_enqueue_script(
+        'bootstrap-js',
+        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js',
+        array('')
+        '4.6.2',
+        'all'
+    );
+}
+    
+    
+
 ?>
