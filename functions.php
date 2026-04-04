b2c8a24915d04b4dfbaba2d29af489e26b9413e1
eldoz <81470244+eldoz@users.noreply.github.com>
2026-04-04 14:16:23 +0200

    EEE

--- functions.php
+++ functions.php
@@ -1,36 +1,58 @@
-<?php
+<?php>
 
-functions ds_theme_assets(){
-    wp_enqueue_script(
-        'ds-script',
-        get_template_directory_uri(). '/js/custom.js',
-        array(),
-        '1.0',
-        'all'
-    ):
+add_action('wp_enqueue_scripts', 'ds_add_bootstrap_cdn');
+
+
+
+function themename_widgets_init(){
+    register_sidebar(array(
+        'name'              => __('Primary Sidebar','theme_name'),
+        'id'                => 'sidebar-1',
+        'before_widget'     =>'<aside id="%1$s" class="widget %2$s">',
+        'after_widget'      => '</aside>',
+        'before_title'      =>'<h3 class="widget-title">',
+        'after_title'       => '</h3>'
+    ));
+}
+add_action('widgets_init','themename_widgets_init');
+
+
+class Foo_Widget extends WP_Widget{
+
+
+
+    public function __construct(){
+        parent::__construct(
+            'foo_widget',
+            'A foo Widget'
+        );
+    }
+
+
+    public function widget($args, $instance){
+        echo $args['before_widget'];
+         echo '<p> hello world </p>';
+         echo '<p> hello grupiii 1 D </p>';
+         echo $args['after_widget'];
+
+
+    }
+    public function form($instance){
+        echo '<p> no options yet</p>';
+
+
+    }
+    public function update($new_instance,$old_instance){
+        return $new_instance;
+    }
 }
 
-add_action('wp_enqueue_scripts','ds_theme_assets');
 
-function ds_add_bootstrap_cdn(){
-
-    wp_enqueue_style(
-        'bootstrap-css',
-        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css',
-        array('')
-        '4.6.2',
-        'all'
-    );
-
-    wp_enqueue_script(
-        'bootstrap-js',
-        'https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js',
-        array('')
-        '4.6.2',
-        'all'
-    );
+function register_foo_widget(){
+    register_widget('Foo_Widget');
 }
-    
-    
+
+
+add_action('widgets_init','register_foo_widget');
 
 ?>
